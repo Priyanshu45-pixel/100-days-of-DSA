9090f0e2aa05d27e87ae313c50f65ce14fd4e893
Priyanshu45-pixel <jhapriyanshu557@gmail.com>
2026-03-22 13:15:11 +0530

    Add files via upload

---
+++ Q50.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+    return root;
+}
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+    if (root->data == key)
+        return 1;
+    if (key < root->data)
+        return search(root->left, key);
+    return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = NULL;
+    int n, val, key, i;
+
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    scanf("%d", &key);
+    if (search(root, key))
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
+    return 0;
+}
