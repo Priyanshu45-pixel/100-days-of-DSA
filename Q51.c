12074c716facb8586f06cacefcfb6f6293a2aa28
Priyanshu45-pixel <jhapriyanshu557@gmail.com>
2026-03-23 14:06:29 +0530

    Add files via upload

---
+++ Q51.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return LCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return LCA(root->right, n1, n2);
+
+    return root;
+}
+
+int main() {
+    int n, x, a, b;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d %d", &a, &b);
+
+    struct Node* ans = LCA(root, a, b);
+    printf("%d", ans->data);
+
+    return 0;
+}
